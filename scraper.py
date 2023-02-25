700bf34b6ecfef8fcb398d0c3b704976f8750c23
Alex Mackenzie <alexmackenzie.wx@gmail.com>
2023-02-25 09:04:35 +0000

    connected to neon database

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import dotenv
 import sys
 import os
+import uuid
 
 dotenv.load_dotenv()
 
@@ -12,6 +13,15 @@ TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
 AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 AIRTABLE_TABLE_ID = os.getenv("AIRTABLE_TABLE_ID")
+NEON_CONNSTRG = os.getenv("NEON_CONNSTRG")
+
+# use sqlalchemy to connect with neon and push tweets there too
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+neon_conn = create_engine(NEON_CONNSTRG)
+
+test = neon_conn.connect()
 
 import tweepy
 
@@ -41,6 +51,7 @@ def track_tweets(keyword:str, count:int):
         tweet_id = str(tweet.id)
         tweet_text = tweet.text
         user_url = "https://twitter.com/"+tweet.user.screen_name
+        nix_tweets_uuid = uuid.uuid4()
 
         table.create(
             {
@@ -50,5 +61,14 @@ def track_tweets(keyword:str, count:int):
                                 }
             )
 
+        # need to insert uuid here
+
+        test.execute(
+            text("INSERT INTO nix_tweets (id, tweetid, tweettext, userurl) VALUES (:id, :tweetid, :tweettext, :userurl)"),
+            [{"id":nix_tweets_uuid, "tweetid": int(tweet_id), "tweettext": tweet_text, "userurl": user_url}]
+            )
+        test.commit()
 
 track_tweets("@floxdevelopment", 5)
+
+print("Done!")
